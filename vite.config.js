e60b8497d24a55779985278ca5c871586db358ca
wesomelo97 <ewertons.lab@gmail.com>
2026-09-15 11:36:14 -0300

    fix: corrige base path do Vite para Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/caderno-dvinho"
-})
+  base: "/caderno-dvinho/",
+});
